04c2b435a6eb66135e8fc8bf656d79956e358e3b
Marcelo Pata <56968177+patamarcelo@users.noreply.github.com>
2022-06-08 11:29:56 -0300

    listagem e ordenacao da data

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import ValuRisk
 
-admin.site.register(ValuRisk)
+# admin.site.register(ValuRisk)
+
+
+@admin.register(ValuRisk)
+class ValuriskAdmin(admin.ModelAdmin):
+    list_display = ('data', 'fechamento')
+    ordering = ('-data',)
+
 
 
