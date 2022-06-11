8eb482094f78e7d8c6c6fc3d884ecf42474f83ae
Marcelo Pata <56968177+patamarcelo@users.noreply.github.com>
2022-06-11 10:24:45 -0300

    no more last days risk

--- api/management/commands/auto_run_riskbrasil.py
+++ api/management/commands/auto_run_riskbrasil.py
@@ -65,7 +65,7 @@ class Command(BaseCommand):
             print(len(query_list))
             count = 0
             list_obj = []
-            for i in query_list[1:]:
+            for i in query_list:
                 data_value = i['Data']
                 abertura = float(i['Abertura'].replace(',', '.'))
                 fechamento = float(i['Fechamento'].replace(',', '.'))
